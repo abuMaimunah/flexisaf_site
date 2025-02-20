038bba158c9927d1e0c439e107d797d0d44b63da
abuMaimunah <133230707+abuMaimunah@users.noreply.github.com>
2025-02-20 11:59:32 +0100

    Added some basic DOM on the FAQ sections

---
+++ script.js
@@ -0,0 +1,16 @@
+const pp1 = document.querySelector("#p11")
+q1.addEventListener("click", function(){
+  pp1.innerHTML = pp1.innerHTML ? "": `Ideally, all assignments are delivered within 24hours of submission`
+})
+const pp2 = document.querySelector("#p22")
+q2.addEventListener("click", function(){
+  pp2.innerHTML = pp2.innerHTML ? "": `Depending on the number of pages, a 5 pages assignment typically cost N5,000`
+})
+const pp3 = document.querySelector("#p33")
+q3.addEventListener("click", function(){
+  pp3.innerHTML = pp3.innerHTML ? "": `Sure, you get a detailed explanation of your assignment`
+})
+const pp4 = document.querySelector("#p44")
+q4.addEventListener("click", function(){
+  pp4.innerHTML = pp4.innerHTML ? "": `Yes, we offer one-one mentorship for better understanding of key concepts`
+})
